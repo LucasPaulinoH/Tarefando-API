69299049eb2d77b53e6cca0f62ee9bac69a1fb91
Lucas Paulino <lucaspaulinoh@hotmail.com>
2024-12-21 09:51:58 -0300

    FIX: fixing expo cors error

--- src/main/java/api/tutoringschool/security/CorsConfig.java
+++ src/main/java/api/tutoringschool/security/CorsConfig.java
@@ -1,15 +1,18 @@
 package api.tutoringschool.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*").allowedOrigins("*");
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedOriginPatterns(CorsConfiguration.ALL);
+
     }
 }
